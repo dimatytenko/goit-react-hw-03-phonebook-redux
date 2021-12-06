dcb248486382882003e5f2c3ab51cf038be0b907
Dima Tytenko <amgpetronass@gmail.com>
2021-12-06 22:00:07 +0200

    Updates

--- static/js/main.9d61d633.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),h=(n(8),n(9),n(0));var s=function(){return Object(h.jsx)("h1",{children:"hello"})};i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.9d61d633.chunk.js.map
